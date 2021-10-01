4c9cc83f54a4632fc961f9cded35441bc347c917
JeffreyRodriigues <jeffrey.rodriigues@gmail.com>
2021-10-01 18:44:48 -0300

    Correção do Exercicío 4

--- exercicios-java/exercicios-java1-10/SegundoExercicio.java
+++ exercicios-java/exercicios-java1-10/SegundoExercicio.java
@@ -7,16 +7,16 @@ public class SegundoExercicio {
 	public static void main(String[] args) {
 		// Leia idade em dias, e mostre em dias,meses e anos 
 		
-		int ano, mes, dias;
+		int ano, mes, dias, contdias;
 		
 		Scanner leia = new Scanner(System.in);
 		
 		System.out.println("Digita a sua idade em dias: ");
-		dias = leia.nextInt();
+		contdias = leia.nextInt();
 		
-		ano = dias/365;
-		mes = (dias%365)/30;
-		dias = (dias%365)%30;
+		ano = contdias/365;
+		mes = (contdias%365)/30;
+		dias = (contdias%365)%30;
 		
 		System.out.println("Você tem " +ano + " anos " +mes + " meses e " +dias + " dias ");
 
