584af05a47733c70b045bd0ef07e9f5687e45b5f
JeffreyRodriigues <jeffrey.rodriigues@gmail.com>
2021-10-27 15:52:46 -0300

    Adicionando pastas para o projeto-blog e projeto-final

---
+++ projeto-final/Treco.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE db_treco;
+
+USE db_treco;
+
+CREATE TABLE `tb_usuario` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`nomecompleto` varchar(255) NOT NULL,
+	`usuario` varchar(255) NOT NULL,
+	`senha` varchar(255) NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tb_categoria` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`nomecategoria` varchar(255) NOT NULL,
+	`descricao` varchar(255) NOT NULL,
+	`ativo` BOOLEAN NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tb_produto` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`nomeproduto` varchar(255) NOT NULL,
+	`descricao` varchar(255) NOT NULL,
+	`preco` DECIMAL(8,2) NOT NULL,
+	`imagem` varchar(255) NOT NULL,
+	`categoria_id` bigint NOT NULL,
+	`usuario_id` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria`(`id`);
+
+ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk1` FOREIGN KEY (`usuario_id`) REFERENCES `tb_usuario`(`id`);
+
+
+
+
